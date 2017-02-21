b5a1c974674d4f3cc3895039eead5ea4e53e3e80
Cara Lynch <snuzzbobble@gmail.com>
2017-02-21 20:36:07 +0000

    Update LennardJones.py

--- LennardJones.py
+++ LennardJones.py
@@ -13,39 +13,109 @@ import math
 import matplotlib.pyplot as pyplot
 import numpy as np
 from ParticleList import ParticleSyst as P
-import MIC as mic
 
 
-# Calculates the force between two particles based on the separation, the cutoff radius and the box dimensions 
-
-def ljforce(system,L,r) :
-    force = np.empty(shape={P.N,3})
-    for i in range(0, P.N + 1):
-	for j in range(0, 3):
-	    force[i,j] = 
+def ljforce(system,L,r_c) :
+	"""
+	Computes the Lennard-Jones force acting on each particle in the system as an (N,3) Numpy array.
 	
-
-	# Computes the vector separation of the particles
-    rvector = mic.vecsep(p1,p2)
-    # Computes the magnitue of the particle separation 
-    rscalar = math.sqrt(np.inner(rvector,rvector))
-        if rscalar > r_c :
-	    return ljforce == 0
-        else : 
-	    return (48*((1/rscalar**14)-(1/(2*rscalar**8))))*rvector
-
-# Calculates the Lennard Jones potential for the particle pair. 			
-def ljpotential(p1,p2,L,r_c) :
-    # Computes the vector separation of the particles
-    rvector = mic.vecsep(p1,p2)
-    # Computes the magnitude of the particle separation 
-    rscalar = math.sqrt(np.inner(rvector,rvector)
-        if rscalar > r_c :
-	    return ljpotential == 0
-        else : 
-	    return (4*((1/rscalar**12)-(1/(r**6))))*rvector
-
-
+	:param system: ParticleSyst object representing the system of N particles
+	:param L: Box dimensions
+	:param r_c: cutoff radius
+	:return force: an (N,3) Numpy array where the ith row is the force experienced by the ith particle
+	"""
+    force = np.empty(shape={P.N,3})
+	
+	# Compute for i th particle
+    for i in range(0, P.N + 1):
+		
+		# With relation to j th particle
+	for j in range(0, P.N+1):
+		
+		if j == i:
+			# not computing the effect the particle has on itself
+			pass
+		else:
+			vecsep = np.empty(shape={1,3})
+			for k in range(0,3):
+				
+				# Calculate the vector separation of the particles adhering to the minimum image convention
+				vecsep[k] = P.position[i,k] - P.position[j,k]
+				
+				# Minimum image convention
+				while abs(vecsep[k]) > L/2 :
+			
+					# If the separation is negative, then the image will be -L/2 in the direction considered
+					if vecsep[k] < 0:
+						vecsep[k] = vecsep[k] - L/2
+				
+					# If the separation is positive, then the image will be +L/2 in the direction considered
+					else:
+						vecsep[k] = vecsep[k] + L/2
+				
+			# Magnitude of vector separation
+			r = math.sqrt(np.inner(vecsep, vecsep)
+			
+			# Cutoff radius
+			if r > r_c :
+				for k in range(0,3):
+				     force[i,k] = force[i,k] + 0
+			# Add this contribution to the force
+			else:
+				for k in range (0,3):
+				     force[i,k] = force[i,k] + (48*((1/r**14) - (1/ (2 * r**8)) ) ) * vecsep[k]
+				      
+	return force
+	
+def ljpotential(system,L,r_c) :
+	"""
+	Computes the Lennard-Jones potential of each particle in the system as an (N,1) Numpy array.
+	
+	:param system: ParticleSyst object representing the system of N particles
+	:param L: Box dimensions
+	:param r_c: cutoff radius
+	:return force: an (N,1) Numpy array where the ith row is the potential of the ith particle
+	"""
+    potential = np.empty(shape={P.N,1})
+	
+	# Compute for i th particle
+    for i in range(0, P.N + 1):
+		
+		# With relation to j th particle
+	for j in range(0, P.N+1):
+		
+		if j == i:
+			# not computing the effect the particle has on itself
+			pass
+		else:
+			vecsep = np.empty(shape={1,3})
+			for k in range(0,3):
+				
+				# Calculate the vector separation of the particles adhering to the minimum image convention
+				vecsep[k] = P.position[i,k] - P.position[j,k]
+				
+				# Minimum image convention
+				while abs(vecsep[k]) > L/2 :
+			
+					# If the separation is negative, then the image will be -L/2 in the direction considered
+					if vecsep[k] < 0:
+						vecsep[k] = vecsep[k] - L/2
+				
+					# If the separation is positive, then the image will be +L/2 in the direction considered
+					else:
+						vecsep[k] = vecsep[k] + L/2
+				
+			# Magnitude of vector separation
+			r = math.sqrt(np.inner(vecsep, vecsep)
+			
+			# Cutoff radius
+			if r > r_c :
+				potential[i] = potential[i] + 0
+			# Add this contribution to the overall potential
+			else:
+				potential[i] = potential[i] + (4*((1/r**12) - (1/ (r**6)) ) )
+				      
+	return potential
 
 	
 
