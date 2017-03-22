5fe630127f521eefbfe55343f59e8cb538deb132
Cara Lynch <snuzzbobble@gmail.com>
2017-03-22 19:07:55 +0000

    Update VelVerlet.py

--- VelVerlet.py
+++ VelVerlet.py
@@ -4,7 +4,7 @@ Velocity Verlet time integration method
 
 Created on Tue Feb 28 12:49:52 2017
 
-@author: Cara Lynch
+Author: Cara Lynch, Marina Ruiz Sanchez-Oro
 """
 
 from ParticleList import ParticleSyst as P
@@ -26,6 +26,11 @@ def totE(syst, boxdim, R_c):
 def VelVerlet(dt, syst, boxdim, R_c):
     """
     Performs one velocity verlet time integration loop
+    
+    :param dt: timestep as a float
+    :param syst: ParticleSyst instance representing the system
+    :param boxdim: box dimensions as a (1,N) Numpy array
+    :param R_c: cutoff radius
     """
     force = lj.ljforce(syst, boxdim, R_c)
     # Update particle position
