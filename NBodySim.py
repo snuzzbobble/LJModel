5d6941bfe1fa6eff288a7c9110f48a7acd407efc
Cara Lynch <snuzzbobble@gmail.com>
2017-03-22 18:56:32 +0000

    Add files via upload

--- NBodySim.py
+++ NBodySim.py
@@ -21,6 +21,9 @@ import MSDplot as MSDplot
 # Input file name from command line
 fileName = str(input("File name: "))
 
+# Will be used to indicate time taken to complete simulation
+systime.clock()
+
 # Option to input timestep if needed
 # dt = float(input("timestep: "))
 
@@ -46,26 +49,16 @@ numstep = int(line1[1]) # number of steps
 
 
 # Set additional parameters
-m = 1.0
-boxdim = (N/rho)**(1./3.)
+m = 1.0 # Mass of particles
 
-# Open system file for writing
+# Create system file which will be used to create a ParticleSyst instance
 systemFile = open("system.in", "w")
-
 systemFile.write(name + " " + str(N) + " " + str(m) + "\n")
-
 for i in range (0, N):
     label = "p" + str(i)
-    pos1 = random.uniform(0.0, boxdim)
-    pos2 = random.uniform(0.0, boxdim)
-    pos3 = random.uniform(0.0, boxdim)
-    systemFile.write(label + " " + str(pos1) + " " + str(pos2) + " " + str(pos3) + " 0.0 0.0 0.0 \n")
-
+    systemFile.write(str(label) +  " 0.0 0.0 0.0 0.0 0.0 0.0 \n")
 systemFile.close()
 
-
-
-   
 # Create ParticleSyst instance
 System = P.createsystem("system.in")
 
@@ -73,22 +66,15 @@ System = P.createsystem("system.in")
 boxdim = md.setInitialPositions(rho, System)
 md.setInitialVelocities(temp, System)
 
-
 k = 0 # timestep number
 
-# Open output file for VMD trajectory information
-VMDfile = open(str(name)+"VMD.xyz", "w")
+# Open output files 
+VMDfile = open(str(name)+"VMD.xyz", "w") # VMD trajectory information
+RDFfile = open(str(name)+"rdf.out", "w") # radial distribution function information
+MSDfile = open(str(name)+"msd.out","w") # mean squared displacement information 
+Energyfile = open(str(name)+"energy.out","w") # energy fluctuations in format: Kinetic Potential Total
 
-# Open output file for radial distribution function information
-RDFfile = open(str(name)+"rdf.out", "w")
-
-# Open output file for mean squared displacement information
-MSDfile = open(str(name)+"msd.out","w")
-
-# Open output file for energy fluctuations in the format: Kinetic Potential Total
-Energyfile = open(str(name)+"energy.out","w")
-
-# Save initial positions to an array to compute the MSD
+# Save initial positions of particles to an array to compute the MSD
 initialpositions = System.position
 
 # Set up data lists for plotting energy
@@ -97,7 +83,7 @@ KEValue = [P.kineticEnergy(System)]
 PEValue = [lj.totPE(System,boxdim,r_c)]
 totEValue = [vv.totE(System, boxdim, r_c)]
              
-# Write values to files
+# Write initial energy values to file
 Energyfile.write("0 " + str.format("{0:.4f}",P.kineticEnergy(System)) + " "  + str.format("{0:.4f}",lj.totPE(System,boxdim,r_c)) + " " + str.format("{0:.4f}",vv.totE(System, boxdim, r_c)) + "\n")
 
 # Print system time at beginning of loop
@@ -105,43 +91,86 @@ print(str(systime.strftime("%H:%M:%S") + " - " + " 0% of loop completed"))
 
 # Start time integration loop
 for i in range(1, numstep):
+    
     # Perform VV time integration
     vv.VelVerlet(dt, System, boxdim, r_c)
     force = lj.ljforce(System,boxdim,r_c)
+    
     # Output trajectory information for VMD file
     trajectory = P.printVMD(System, k)
     VMDfile.write(trajectory)
     
-    # Output energy information for energy file
-    tValue.append(i)
-    KEValue.append(P.kineticEnergy(System))
-    PEValue.append(lj.totPE(System,boxdim,r_c))
-    totEValue.append(vv.totE(System, boxdim, r_c))
     
-    Energyfile.write(str(i) + " " + str.format("{0:.4f}",P.kineticEnergy(System)) + " "  + str.format("{0:.4f}",lj.totPE(System,boxdim,r_c)) + " " + str.format("{0:.4f}",vv.totE(System, boxdim, r_c)) + "\n")
-    
-    # RDF histogram and MSD calculation
-    for l in range(0, System.N):
-        # Output radial distances for RDF
-        hist.particledistances(System, RDFfile,boxdim,l)
-        # MSD calculation
-        displacementarray = System.position - initialpositions
+    # For a small enough number of steps, get Energy, RDF and MSD data for every step
+    if numstep <= 1000:
         
-    MSDtimesN = 0.0
-    # Sum over all squared displacements
-    for m in range(0, System.N):
-        MSDtimesN += np.inner(displacementarray[m],displacementarray[m])
-        # Divide by N
-    MSD = MSDtimesN/System.N
-            
-    # Add to MSD file with format: timestep MSD
-    MSDfile.write(str(i)+ " " + str.format("{0:.4f}",MSD) + "\n")
+        # Output energy information for energy file
+        tValue.append(i)
+        KEValue.append(P.kineticEnergy(System))
+        PEValue.append(lj.totPE(System,boxdim,r_c))
+        totEValue.append(vv.totE(System, boxdim, r_c))
     
+        Energyfile.write(str(i) + " " + str.format("{0:.4f}",P.kineticEnergy(System)) + " "  + str.format("{0:.4f}",lj.totPE(System,boxdim,r_c)) + " " + str.format("{0:.4f}",vv.totE(System, boxdim, r_c)) + "\n")
+        
+        # RDF histogram and MSD calculation
+        for l in range(0, System.N):
+        
+            # Output radial distances for RDF
+            hist.particledistances(System, RDFfile,boxdim,l)
+            
+            # MSD calculation
+            displacementarray = System.position - initialpositions
+        
+        MSDtimesN = 0.0
+        # Sum over all squared displacements
+        for m in range(0, System.N):
+            MSDtimesN += np.inner(displacementarray[m],displacementarray[m])
+        
+        # Divide by N
+        MSD = MSDtimesN/System.N
+            
+        # Add to MSD file with format: timestep MSD
+        MSDfile.write(str(i)+ " " + str.format("{0:.4f}",MSD) + "\n")
+         
+    # For a large number of steps, only get Energy, RDF and MSD data every 4th step
+    else:
+        if i%4.0 == 0:
+            
+            # Output energy information for energy file
+            tValue.append(i)
+            KEValue.append(P.kineticEnergy(System))
+            PEValue.append(lj.totPE(System,boxdim,r_c))
+            totEValue.append(vv.totE(System, boxdim, r_c))
+    
+            Energyfile.write(str(i) + " " + str.format("{0:.4f}",P.kineticEnergy(System)) + " "  + str.format("{0:.4f}",lj.totPE(System,boxdim,r_c)) + " " + str.format("{0:.4f}",vv.totE(System, boxdim, r_c)) + "\n")
+            
+            # RDF histogram and MSD calculation
+            for l in range(0, System.N):
+        
+                # Output radial distances for RDF
+                hist.particledistances(System, RDFfile,boxdim,l)
+                
+                # MSD calculation
+                displacementarray = System.position - initialpositions
+        
+            MSDtimesN = 0.0
+            # Sum over all squared displacements
+            for m in range(0, System.N):
+                MSDtimesN += np.inner(displacementarray[m],displacementarray[m])
+            # Divide by N
+            MSD = MSDtimesN/System.N
+            
+            # Add to MSD file with format: timestep MSD
+            MSDfile.write(str(i)+ " " + str.format("{0:.4f}",MSD) + "\n")
+            
+        else:
+            pass
+    
+
     # Increase timestep number tracker
     k += 1
     
-    # Give percentage of completion of simulation
-    
+    # Give percentage of completion of simulation every 5%
     if i%(numstep/20.0) == 0:
         print(str(systime.strftime("%H:%M:%S") + " - " + str(int(i*100/numstep)) + "% of loop completed"))
 
@@ -150,6 +179,20 @@ VMDfile.close()
 RDFfile.close()
 MSDfile.close()
 Energyfile.close()
+
+print(str(systime.strftime("%H:%M:%S") + " - 100% of loop completed"))
+
+# Indicate simulation time before graph plotting
+print("It took "+ str(systime.clock()) + " seconds to compute the time evolution of an " + str(N) + " body system over " + str(numstep) + " steps")
+
+
+# plot graph of mean squared distance evolution (MSD)
+MSDplot.plot(str(name)+"msd.out",name)
+
+    
+# Histogram for RDF function
+hist.histogram(str(name)+"rdf.out",name, System, rho, numstep)
+
 
 # plot graph of energy fluctuations
 pyplot.plot(tValue,totEValue, "g", label="Total Energy")
@@ -160,15 +203,7 @@ pyplot.title("Energy over timesteps")
 pyplot.xlabel("Time step number")
 pyplot.ylabel("Energy ")
 pyplot.savefig(str(name)+'Energyevolution')
-
-
 pyplot.show()
 
-# plot graph of mean squared distance evolution (MSD)
-MSDplot.plot(str(name)+"msd.out",name)
-
-    
-# Histogram for RDF function
-hist.histogram(str(name)+"rdf.out",name)
 
 
--- histogram.py
+++ histogram.py
@@ -16,13 +16,12 @@ from ParticleList import ParticleSyst as P
 
 def particledistances(syst, fileName,boxdim, k):
     """
-    Collects distances between pairs of particles at a specific point in time with respect to the k th particle and exports into a file.
+    Collects distances between pairs of particles at a specific point in time with respect to the k th particle (the reference particle) and exports into a file.
     
-    Has to be integrated into VV code
     :param syst: N body system represented as a ParticleSyst instance
     :param fileName: name of file which the trajectories will be exported to as a string
-    :param k: index of reference particle
-    :param boxdim: boxdimensions
+    :param boxdim: box dimensions as a Numpy array
+    :param k: index of reference particle as an integer
     """
    
     # Compute radial distances from reference particle k
@@ -40,29 +39,41 @@ def particledistances(syst, fileName,boxdim, k):
 
 
 
-def histogram(fileName, name):
+def histogram(fileName, name, syst, rho, numstep):
+    """
+    Plots a normalised histogram of the radial distribution function.
+    
+    :param fileName: name of file with RDF data as a string
+    :param name: name of system as a string
+    :param syst: ParticleSyst instance representing the system
+    :param rho: density of system
+    :param numstep: number of timesteps
+    """
     
     # Open the file of radial distances for reading
     fileIn = open(fileName, "r")
+
+    # Make array of distances
     distances = fileIn.readlines()
-    # Count number of elements in distances list to create array
-    num = len(distances)
-    rdfArray = np.empty(shape=(num))
+    rdfArray = np.array(distances, dtype = float)
     
-    # Assign list values as floats into array
-    for i in range(0,num):
-        rdfArray[i] = float(distances[i])
-    
-        
-    rdf = rdfArray
     
     # Create and plot normalised histogram
-    x,bins,p=plt.hist(rdf, normed=1)
+    hist,bin_edges=np.histogram(rdfArray, bins = 100, density = False)
+    dr = bin_edges[1]-bin_edges[0]
     
-    plt.legend()
+    histnormalised = hist
+    for i in range(0, hist.size):
+        histnormalised[i] = hist[i]/(4*math.pi*bin_edges[i]**(2)*rho*syst.N*numstep*dr)
+    
+    xdata = np.delete(bin_edges,hist.size-1)
+    
+    plt.plot(xdata,histnormalised, "g", label="rdf")
     plt.title("Histogram of radial distribution function for a " + str(name))
     plt.xlabel("Radial distance")
     plt.ylabel("Frequency density")
     plt.savefig(str(name)+'Histogram')
     plt.show()
+
+
     
